cdc8bd7b1065587d2ad860b43148843b15307503
Volodymyr Basok <volodymyrquo@gmail.com>
2024-05-16 19:34:20 +0300

    Add create dir upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express' 
+import fs from 'fs'
 import mongoose from 'mongoose';
 import cors from 'cors'
 import { registerValidation,loginValidation, postCreateValidation} from './validations.js';
@@ -13,6 +14,9 @@ mongoose.connect(process.env.MONGODB_URI)
 const app = express();
 const storage = multer.diskStorage({
     destination:(_,__,cb)=>{
+        if(!fs.existsSync('uploads')){
+            fs.mkdirSync('uploads')
+        }
         cb(null,'uploads')
     },
     filename:(_,file,cb)=>{
